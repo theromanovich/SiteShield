a4c256ae28883ed015e62d9c08323f39867204e2
Vlad Khomenko <theromanovich03@gmail.com>
2023-11-26 15:46:11 +0200

    added swagger output of auth dtos

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   @Post('sign-up')
-  signUp() {}
+  @ApiCreatedResponse()
+  signUp(@Body() body: SignUpBodyDto) {}
 
   @Post('sign-in')
+  @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
-  signIn() {}
+  signIn(@Body() body: SignInBodyDto) {}
 
   @Post('sign-out')
   @HttpCode(HttpStatus.OK)
   signOut() {}
 
   @Get('session')
+  @ApiOkResponse({
+    type: GetSessionInfoDto
+  })
   getSessionInfo() {}
 }
