3f78873a0ad76210be17d29d1924d4a209c3cbad
Vlad Khomenko <theromanovich03@gmail.com>
2023-11-26 15:26:15 +0200

    cookie service

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PasswordService } from './password/password.service';
+import { PasswordService } from './password.service';
+import { CookieService } from './cookie.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PasswordService]
+  providers: [AuthService, PasswordService, CookieService]
 })
 export class AuthModule {}
