f0c530d8241d44e0ad53f18329c33c01f9652c20
Vlad Khomenko <theromanovich03@gmail.com>
2023-11-26 14:46:11 +0200

    base swagger output

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
 import { AppService } from './app.service';
+
+class HelloWorldDto {
+  @ApiProperty()
+  message: string
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDto
+  })
+  getHello(): HelloWorldDto {
+    return { message: this.appService.getHello() }
   }
 }
