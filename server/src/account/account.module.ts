9a6faccaddc0702ca05e3c03c1fe4cd5793280f3
Vlad Khomenko <theromanovich03@gmail.com>
2023-11-29 20:38:30 +0200

    added missed import

--- server/src/account/account.module.ts
+++ server/src/account/account.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from 'src/db/db.module';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 
 @Module({
+  imports: [DbModule],
   controllers: [AccountController],
   providers: [AccountService],
   exports: [AccountService],
