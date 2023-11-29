eddfc5a5bc9452440518215b9153c4f1e4c0f1a2
Vlad Khomenko <theromanovich03@gmail.com>
2023-11-29 20:25:31 +0200

    getAccount and patchAccount methods

--- server/src/account/account.controller.ts
+++ server/src/account/account.controller.ts
@@ -1,10 +1,32 @@
-import { Controller, Get, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { GetSessionInfoDto } from 'src/auth/dto';
+import { SessionInfo } from 'src/auth/session-info.decorator';
+import { AccountService } from './account.service';
+import { AccoutDto, PatchAccountDto } from './dto';
 
 @Controller('account')
+@UseGuards(AuthGuard)
 export class AccountController {
+  constructor(private accountService: AccountService) {}
+
   @Get()
-  getAccount() {}
+  @ApiOkResponse({
+    type: AccoutDto,
+  })
+  getAccount(@SessionInfo() session: GetSessionInfoDto): Promise<AccoutDto> {
+    return this.accountService.getAccount(session.id);
+  }
 
   @Patch()
-  patchAccount() {}
+  @ApiOkResponse({
+    type: AccoutDto,
+  })
+  patchAccount(
+    @Body() body: PatchAccountDto,
+    @SessionInfo() session: GetSessionInfoDto,
+  ): Promise<AccoutDto> {
+    return this.accountService.patchAccount(session.id, body);
+  }
 }
--- server/src/account/account.service.ts
+++ server/src/account/account.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
+import { PatchAccountDto } from './dto';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+  constructor(private db: DbService) {}
+
+  async getAccount(userId: number) {
+    return this.db.account.findUniqueOrThrow({ where: { ownerId: userId } });
+  }
+
+  async patchAccount(userId: number, patch: PatchAccountDto) {
+    return this.db.account.update({
+      where: {
+        ownerId: userId,
+      },
+      data: { ...patch },
+    });
+  }
+}
