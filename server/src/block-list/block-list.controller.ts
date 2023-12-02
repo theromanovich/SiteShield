bfefb74ec68b173761e4ff98cd894f7b936f40a0
Vlad Khomenko <theromanovich03@gmail.com>
2023-12-02 13:42:50 +0200

    added guard for block-list controller

--- server/src/block-list/block-list.controller.ts
+++ server/src/block-list/block-list.controller.ts
@@ -7,25 +7,44 @@ import {
   ParseIntPipe,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
-import { AddBlockItemDto, BlockItemDto, BlockListDto, BlockListQueryDto } from './dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { GetSessionInfoDto } from 'src/auth/dto';
+import { SessionInfo } from 'src/auth/session-info.decorator';
+import {
+  AddBlockItemDto,
+  BlockItemDto,
+  BlockListDto,
+  BlockListQueryDto,
+} from './dto';
 
 @Controller('block-list')
+@UseGuards(AuthGuard)
 export class BlockListController {
   @Get()
   @ApiOkResponse({
     type: BlockListDto,
   })
-  getLst(@Query() query: BlockListQueryDto) {}
+  getLst(
+    @Query() query: BlockListQueryDto,
+    @SessionInfo() session: GetSessionInfoDto,
+  ) {}
 
   @Post('item')
   @ApiCreatedResponse({
     type: BlockItemDto,
   })
-  addBlockList(@Body() body: AddBlockItemDto) {}
+  addBlockList(
+    @Body() body: AddBlockItemDto,
+    @SessionInfo() session: GetSessionInfoDto,
+  ) {}
 
   @Delete('item/:id')
   @ApiOkResponse()
-  removeBlockItem(@Param(ParseIntPipe) id: number) {}
+  removeBlockItem(
+    @Param(ParseIntPipe) id: number,
+    @SessionInfo() session: GetSessionInfoDto,
+  ) {}
 }
