4a00152d5efde701a91d79c6601162c799b19d50
Vlad Khomenko <theromanovich03@gmail.com>
2023-12-02 13:40:52 +0200

    import db module

--- server/src/block-list/block-list.module.ts
+++ server/src/block-list/block-list.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from 'src/db/db.module';
 import { BlockListController } from './block-list.controller';
 import { BlockListService } from './block-list.service';
 
 @Module({
+  imports: [DbModule],
   controllers: [BlockListController],
   providers: [BlockListService],
 })
