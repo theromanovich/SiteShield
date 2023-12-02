91bce86e436bf4454526a651b02339ce094ed5f3
Vlad Khomenko <theromanovich03@gmail.com>
2023-12-02 13:36:25 +0200

    block list crud

--- server/src/block-list/block-list.controller.ts
+++ server/src/block-list/block-list.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { AddBlockItemDto, BlockItemDto, BlockListDto } from './dto';
 
 @Controller('block-list')
-export class BlockListController {}
+export class BlockListController {
+  @Get()
+  @ApiOkResponse({
+    type: BlockListDto,
+  })
+  getLst() {}
+
+  @Post('item')
+  @ApiCreatedResponse({
+    type: BlockItemDto,
+  })
+  addBlockList(@Body() body: AddBlockItemDto) {}
+
+  @Delete('item/:id')
+  @ApiOkResponse()
+  removeBlockItem(@Param(ParseIntPipe) id: number) {}
+}
