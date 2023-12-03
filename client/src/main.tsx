e208cd9f6f0c534cadfe47ee3fd34ec18c3e7904
Vlad Khomenko <theromanovich03@gmail.com>
2023-12-03 14:31:25 +0200

    project configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,8 @@ import { App } from '@/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-           <App />
+    <App />
   </React.StrictMode>,
 );
