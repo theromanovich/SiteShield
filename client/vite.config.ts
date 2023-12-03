2137f5b3032dc9f1a5a2da9a8deded5c5362d89c
Vlad Khomenko <theromanovich03@gmail.com>
2023-12-03 16:03:00 +0200

    cors redirects

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import cors from 'cors';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+
+    // @ts-expect-error
+    middlewares: [
+      cors(), // Добавляем middleware для обработки CORS
+    ],
+  },
 });
