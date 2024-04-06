8a805b2fd7d632a0c1d62ae1f2757010624547ab
theromanovich <theromanovich03@gmail.com>
2024-04-06 16:03:19 +0300

    clean up

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,15 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/react-router';
+import { useSessionQuery } from '@/entities/session';
+import { LogoIcon } from '@/shared/ui/logo';
+import { Profile } from '@/widgets/profile';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 
 function Layout() {
+  const { data: session } = useSessionQuery();
   return (
     <div>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/sign-up" className="[&.active]:font-bold">
-          SignUp
-        </Link>
+      <div className="p-2 flex gap-2 justify-between">
+        <LogoIcon className={session ? '' : 'mx-auto'} />
+        {session ? <Profile /> : null}
       </div>
       <Outlet />
     </div>
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -3,13 +3,11 @@ import { protectedPage } from '@/features/auth';
 import { AddBlockItemForm } from '@/features/block-list/ui/add-block-item-form';
 import { BlockList } from '@/features/block-list/ui/block-list';
 import ToggleBlockingButton from '@/features/toggle-blocking/ui/toggle-blocking-button';
-import { Profile } from '@/widgets/profile';
 import { createRoute } from '@tanstack/react-router';
 
 export function HomePage() {
   return (
     <div>
-      <Profile />
       <ToggleBlockingButton />
 
       <div className="mt-10">
