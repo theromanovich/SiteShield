2a59e1d933be74a7acf0ae777d2a727d5c748600
theromanovich <theromanovich03@gmail.com>
2024-04-06 14:14:01 +0300

    profile widget

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,14 @@
 import { RootLayout } from '@/app/layout';
 import { SignOutButton, protectedPage } from '@/features/auth';
+import ToggleBlockingButton from '@/features/toggle-blocking/ui/toggle-blocking-button';
+import { Profile } from '@/widgets/profile';
 import { createRoute } from '@tanstack/react-router';
 
 export function HomePage() {
   return (
     <div>
-      <SignOutButton />
+      <Profile />
+      <ToggleBlockingButton />
     </div>
   );
 }
---
+++ client/src/widgets/profile/index.tsx
@@ -0,0 +1,17 @@
+import { useSessionQuery } from "@/entities/session";
+import { SignOutButton } from "@/features/auth";
+
+export function Profile() {
+    const { data: session } = useSessionQuery()
+
+    if (!session) {
+        return null
+    }
+
+    return (
+        <div className="flex items-center gap-2">
+            {session.email}
+            <SignOutButton />
+        </div>
+    )
+}
