991e58a133aa108f0281419ed5918eed79ce0c77
theromanovich <theromanovich03@gmail.com>
2024-04-07 13:43:23 +0300

    block list entity

---
+++ extension/src/entities/block-list/queries.ts
@@ -0,0 +1,37 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import {
+  blockListControllerAddBlockItem,
+  blockListControllerGetList,
+  blockListControllerRemoveBlockItem,
+} from '@/shared/api/generated';
+
+const blockListKey = ['block-list'] as unknown[];
+
+export function useBlockListQuery({ q }: { q?: string }) {
+  return useQuery({
+    queryKey: blockListKey.concat([{ q }]),
+    queryFn: () => blockListControllerGetList({ q }),
+    placeholderData: (previousData) => previousData,
+  });
+}
+
+export function useAddBlockItemMutation() {
+  const queyrClient = useQueryClient();
+  return useMutation({
+    mutationFn: blockListControllerAddBlockItem,
+    async onSettled() {
+      await queyrClient.invalidateQueries({ queryKey: blockListKey });
+    },
+  });
+}
+
+export function useRemoveBlockItemMutation() {
+  const queyrClient = useQueryClient();
+  return useMutation({
+    mutationFn: blockListControllerRemoveBlockItem,
+    async onSettled() {
+      await queyrClient.invalidateQueries({ queryKey: blockListKey });
+    },
+  });
+}
