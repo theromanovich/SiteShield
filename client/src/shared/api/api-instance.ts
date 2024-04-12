8de2607d19931f036c996f79efaab04a2107f11c
theromanovich <theromanovich03@gmail.com>
2024-04-12 16:28:56 +0300

    revert

--- client/src/shared/api/api-instance.ts
+++ client/src/shared/api/api-instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 export const apiInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
