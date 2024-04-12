6bdef9cc7ee989882cb64671ced62b0a200c8833
theromanovich <theromanovich03@gmail.com>
2024-04-12 15:20:48 +0300

    remove dev server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,14 @@
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import path from "path"
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), TanStackRouterVite()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
