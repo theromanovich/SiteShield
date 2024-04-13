411d2f93e2f4a6cbf1c1511f640ce6c7346b35e8
theromanovich <theromanovich03@gmail.com>
2024-04-13 21:26:35 +0300

    remove toggling button from extension

--- extension/src/pages/home.tsx
+++ extension/src/pages/home.tsx
@@ -1,4 +1,3 @@
-import { ToggleBlockingButton } from '@/features/toggle-blocking/ui/toggle-blocking-button';
 import { ADMIN_SIGN_IN_URL } from '@/shared/constants';
 import { createTab } from '@/shared/lib/browser';
 import { Button } from '@/shared/ui/button';
@@ -8,7 +7,6 @@ export function HomePage() {
   return (
     <div className="p-4 w-92 flex flex-col gap-3 items-center w-48">
       <LogoIcon />
-      <ToggleBlockingButton />
       <Button onClick={() => createTab(ADMIN_SIGN_IN_URL)}>Manage extension</Button>
     </div>
   );
