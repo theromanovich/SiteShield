71088edd102c6a9d2271d57056040022ef5cc6ff
theromanovich <theromanovich03@gmail.com>
2024-04-13 21:39:33 +0300

    bugfix with showing header on auth pages

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,17 +1,22 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { Outlet, createRootRoute, useRouterState } from '@tanstack/react-router';
 import clsx from 'clsx';
 
 import { useSessionQuery } from '@/entities/session';
+import { ROUTES } from '@/shared/constants/routes';
 import { LogoIcon } from '@/shared/ui/logo';
 import { Header } from '@/widgets/header';
 
 function Layout() {
   const { data: session } = useSessionQuery();
 
+  const router = useRouterState();
+
+  const isAuth = session && router.location.pathname === ROUTES.Home;
+
   return (
     <div className="px-4 py-2 max-w-screen-lg mx-auto">
       <div className={clsx('flex gap-2', session ? 'justify-between' : 'justify-center')}>
-        {session ? <Header /> : <LogoIcon />}
+        {isAuth ? <Header /> : <LogoIcon />}
       </div>
       <Outlet />
     </div>
--- extension/src/pages/unauthorized.tsx
+++ extension/src/pages/unauthorized.tsx
@@ -7,7 +7,7 @@ export function UnAuthorized() {
   return (
     <div className="p-4 flex flex-col gap-3 w-48 items-center">
       <LogoIcon />
-      <h2 className="text-xl">You are not authorized!</h2>
+      <h2 className="text-xl text-center">You are not authorized!</h2>
       <Button onClick={() => createTab(ADMIN_SIGN_IN_URL)}>Sign In</Button>
     </div>
   );
